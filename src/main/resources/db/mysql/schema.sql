26197c9615240148e8b60aab7a6ca45a74f4da14
xricherenkova <79928904+xricherenkova@users.noreply.github.com>
2021-12-16 15:59:00 +0400

    fix

--- src/main/resources/db/mysql/schema.sql
+++ src/main/resources/db/mysql/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS vets (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  age INTEGER(4),
+  age INT(3),
   INDEX(last_name)
 ) engine=InnoDB;
 
